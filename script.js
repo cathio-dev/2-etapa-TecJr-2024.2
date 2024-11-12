db991f1bbe3bf251c8ecd17dc1cb8871c89ec555
cathio-dev <ramalho.marcos7@gmail.com>
2024-11-11 22:47:04 -0300

    333shop

--- script.js
+++
@@ -1,8 +0,0 @@
-function openModal(productName) {
-    document.getElementById('modal-text').innerText = productName + ' - Descrição do produto aqui.';
-    document.getElementById('modal').style.display = 'block';
-}
-
-function closeModal() {
-    document.getElementById('modal').style.display = 'none';
-}
